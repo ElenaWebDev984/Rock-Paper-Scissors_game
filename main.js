89c70ac8de086d992798601fa463519ef5d295fc
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-01-17 20:21:55 +0100

    add hasPlayerWonTheRound

--- main.js
+++ main.js
@@ -2,5 +2,17 @@ function getRandomComputerResult() {
     const options = ["Rock", "Paper", "Scissors"];
     const randomIndex = Math.floor(Math.random() * options.length);
     return options[randomIndex];
-  }
-  console.log(getRandomComputerResult());
+}
+console.log(getRandomComputerResult());
+
+let playerScore = 0;
+let computerScore = 0;
+
+function hasPlayerWonTheRound(player, computer) {
+  return (
+    (player === "Rock" && computer === "Scissors") ||
+    (player === "Scissors" && computer === "Paper") ||
+    (player === "Paper" && computer === "Rock"));
+}
+console.log(hasPlayerWonTheRound("Rock", "Scissors")); 
+console.log(hasPlayerWonTheRound("Scissors", "Rock")); 
