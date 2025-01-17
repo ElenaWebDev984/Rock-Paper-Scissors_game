2a80ae6c7c1890202a687db182ed729f9e0fb785
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-01-17 20:34:59 +0100

    add getRoundResults

--- main.js
+++ main.js
@@ -5,9 +5,6 @@ function getRandomComputerResult() {
 }
 console.log(getRandomComputerResult());
 
-let playerScore = 0;
-let computerScore = 0;
-
 function hasPlayerWonTheRound(player, computer) {
   return (
     (player === "Rock" && computer === "Scissors") ||
@@ -15,4 +12,24 @@ function hasPlayerWonTheRound(player, computer) {
     (player === "Paper" && computer === "Rock"));
 }
 console.log(hasPlayerWonTheRound("Rock", "Scissors")); 
-console.log(hasPlayerWonTheRound("Scissors", "Rock")); 
+console.log(hasPlayerWonTheRound("Scissors", "Rock"));
+
+let playerScore = 0;
+let computerScore = 0;
+ 
+
+function getRoundResults(userOption) {
+  const computerResult = getRandomComputerResult();
+
+    if (hasPlayerWonTheRound(userOption, computerResult)) {
+      playerScore++;
+      return `Player wins! ${userOption} beats ${computerResult}`;
+    } else if (computerResult === userOption) {
+      return `It's a tie! Both chose ${userOption}`
+    } else {
+      computerScore++;
+      return `Computer wins! ${computerResult} beats ${userOption}`;
+    }
+}
+console.log(getRoundResults("Rock"));
+console.log("Player Score: ", playerScore, "Computer Score: ", computerScore);
